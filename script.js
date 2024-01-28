adacd6417b8a9af8ea5e3a6f9691ef9ffb23043c
fadyxnoob <danishmehbob886@gmail.com>
2024-01-28 19:31:30 +0500

    commited

---
+++ script.js
@@ -0,0 +1,92 @@
+// turn Pages
+const pageTurnBtn = document.querySelectorAll(".nextprev-btn");
+pageTurnBtn.forEach((el, index) => {
+  el.onclick = () => {
+    const pageTurnId = el.getAttribute("data-page");
+    const pageTurn = document.getElementById(pageTurnId);
+
+    if (pageTurn.classList.contains("turn")) {
+      pageTurn.classList.remove("turn");
+      setTimeout(() => {
+        pageTurn.style.zIndex = 20 - index;
+      }, 500);
+    } else {
+      pageTurn.classList.add("turn");
+      setTimeout(() => {
+        pageTurn.style.zIndex = 20 + index;
+      }, 500);
+    }
+  };
+});
+
+// contact me btn (to go to conntact me page)
+const pages = document.querySelectorAll(".book-page.page-right");
+const contactMeBtn = document.querySelector(".btn.contact-me");
+
+contactMeBtn.onclick = () => {
+  pages.forEach((page, index) => {
+    setTimeout(() => {
+      page.classList.add("turn");
+
+      setTimeout(() => {
+        page.style.zIndex = 10 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+};
+
+//  function (go back to the home page )
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseindex() {
+  pageNumber--;
+  if (pageNumber < 0) {
+    pageNumber = totalPages - 1;
+  }
+}
+
+// back Profile btn function
+const backProfleBtn = document.querySelector(".back-profile");
+backProfleBtn.onclick = () => {
+  pages.forEach((_, index) => {
+    setTimeout(() => {
+      reverseindex();
+      pages[pageNumber].classList.remove("turn");
+
+      setTimeout(() => {
+        reverseindex();
+        pages[pageNumber].style.zIndex = 10 + index;
+      }, 500);
+    }, (index + 1) * 200 + 100);
+  });
+};
+
+// opening animation
+const coverRight = document.querySelector(".cover.cover-right");
+const pageLeft = document.querySelector("book-page.page-left");
+setTimeout(() => {
+  coverRight.classList.add("turn");
+}, 2100);
+
+setTimeout(() => {
+  coverRight.style.zIndex = -1;
+}, 2800);
+
+// opening animation (left animation or profile page animation)
+setTimeout(() => {
+  pageLeft.style.zIndex = 20;
+}, 3200);
+
+// opening home page
+pages.forEach((_, index) => {
+  setTimeout(() => {
+    reverseindex();
+    pages[pageNumber].classList.remove("turn");
+
+    setTimeout(() => {
+      reverseindex();
+      pages[pageNumber].style.zIndex = 10 + index;
+    }, 500);
+  }, (index + 1) * 200 + 2100);
+});
